29bc850f663d4ac7c1f45fa8d563774c823d59f0
Andree_akash <110694207+Andreeakash@users.noreply.github.com>
2026-08-31 17:45:49 +0530

    initial commit

---
+++ JsBasic/index.js
@@ -0,0 +1,73 @@
+// console.log("ye rha mera pehle code");
+
+// // ye rha ek line k andr complete 
+// console.log("ye rha aab alag se sepration ka code");
+
+// // ye rha multi line k andr print krna kuch v 
+// console.log(`aab mai 
+//             ye kr rha
+//             hu ki multi 
+//             line print 
+//             jo jaa`);
+
+
+// let a=5;
+// let b=3.14;
+// let c=true;
+// let d="aman";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// let value1=3.12;// scope : block scoped 
+// var value='akash';// scope: function scoped
+// console.log(value);
+
+
+// let firstName = "Aman";     // ✅ standard practice
+// let first_name = "Aman";    // works, but JS mein convention nahi hai
+
+
+
+// // objects
+
+// let person={
+//      firstName:"Aman",
+//      secondName:"kumar",
+//      Age:25
+
+// };
+// //  object access using Dot notation
+// console.log(person.firstName);
+// console.log(person.secondName);
+// console.log(person.Age);
+
+// //  object access using bracket Notation
+// console.log(person["firstName"]);
+// console.log(person["secondName"]);
+// console.log(person["Age"]);
+
+// let key = "firstName";
+// console.log(person[key]);  // ✅ ye chalega, kyunki key ek defined variable hai jiski value "firstName" (string) hai
+
+// // for loops
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// // while loops
+// let y=1;
+// while(y<10){
+//     console.log(y);
+//     y++;
+// }
+
+// // DO-while-loop
+
+// let x=0;
+// do{
+//     console.log(x);
+//     x++;
+// }while(x<10);
