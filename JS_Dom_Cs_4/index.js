c33c6d32a3e4df4ec817db4f4c5e1c3da6c42c1b
Andree_akash <110694207+Andreeakash@users.noreply.github.com>
2026-09-11 08:18:10 +0530

    fetchAPI and closure

--- JS_Dom_Cs_4/index.js
+++ JS_Dom_Cs_4/index.js
@@ -78,19 +78,124 @@
 // meraPromise1.catch((error) => console.log(error.message));  
 // // Sirf clean message (user ko dikhane ke liye ya simple logging ke liye)
 
-let firstPromise = new Promise(function(resolve, reject){
-    setTimeout(()=>console.log("ye rha first promise"),3000);
-    resolve(true);
-});
+// let firstPromise = new Promise(function(resolve, reject){
+//     setTimeout(()=>console.log("ye rha first promise"),3000);
+//     resolve(true);
+// });
 
-let thirdPromise=firstPromise.then(()=>{
-    let secondpromise=new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            console.log("second promise 2 started")
-        },2000);
-        resolve("second promise to resolved");
-    })
-    return secondpromise;
-});
+// let thirdPromise=firstPromise.then(()=>{
+//     let secondpromise=new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("second promise 2 started")
+//         },2000);
+//         resolve("second promise to resolved");
+//     })
+//     return secondpromise;
+// });
 
-thirdPromise.then((value)=>console.log(value));
+// thirdPromise.then((value)=>console.log(value));
+
+
+// CREATION OF ASYNC FUNCTION -> ASYNC FUNCTION HUMesha promise return karta hai
+// async function abcd() {
+//     return "hello how are you ";
+// } 
+
+// async function utility() {
+//     let delhiMausam= new Promise((resolve, reject) =>{
+//       setTimeout(() => {
+//         resolve("delhi me Bahut garmi ");
+        
+//       },5000);
+//     });
+
+//    let hydMausam= new Promise((resolve, reject)=>{
+//      setTimeout(() => {
+//         resolve("hydredabad ka mausam cool hai ");
+        
+//       },3000);
+//     });
+
+//     let dM=delhiMausam;
+//     let hM=hydMausam;
+
+//     return[dM,hM];
+    
+// }
+
+
+
+// async function utility() {
+//     let delhiMausam= new Promise((resolve, reject) =>{
+//       setTimeout(() => {
+//         resolve("delhi me Bahut garmi ");
+        
+//       },5000);
+//     });
+
+//    let hydMausam= new Promise((resolve, reject)=>{
+//      setTimeout(() => {
+//         resolve("hydredabad ka mausam cool hai ");
+        
+//       },3000);
+//     });
+
+//     let dM=await delhiMausam;
+//     let hM=await hydMausam;
+
+//     return[dM,hM];
+    
+// }
+
+// Calling fetch api
+
+// async function utility(){
+//     let content = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//     let output= await content.json();
+//     console.log(output);
+
+// }
+
+// data sending to server
+
+// async function helper(){
+
+//     let option={
+//         method:'post',
+//         body:JSON.stringify({
+//             title:"aman",
+//             body: "fit body",
+//             userId:1006
+//         }),
+//         header:{
+//             'content-type':'application/json;charset=UTF-8'
+//         },
+//     };
+//     let content =await fetch ('https://jsonplaceholder.typicode.com/posts',option);
+//     let response=content.json();
+//     return response;
+// }
+// async function utility(){
+//     let ans=await helper();
+//     console.log(ans);
+// }
+
+// console.log(utility());
+
+
+// Closure
+
+function makeFunc(){
+    let name="aman";
+    function displayName(){
+        console,log(name);
+    }
+    return displayName;
+}
+
+const myFunc=makeFunc();
+console.log(myFunc);
+
+
+
+
